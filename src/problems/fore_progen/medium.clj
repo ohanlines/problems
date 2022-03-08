23e06b7da28938f15bcf85e7f7e94bdaebcda39e
ohanlines <rauhanhouse7@gmail.com>
2022-03-08 13:00:15 +0700

    update solution in medium.clj

--- src/problems/fore_progen/medium.clj
+++ src/problems/fore_progen/medium.clj
@@ -1,1 +1,10 @@
 (ns problems.fore-progen.medium)
+
+(defn happy-numbers [x]
+  (let [f1 (fn [a] (* a a))
+        f2 (fn [s] (map read-string (re-seq #"\d" (str s))))]
+    (loop [res x]
+      (cond
+        (= res 1) true
+        (<= x 3)  false
+        :else     (recur (reduce + (map f1 (f2 res))))))))
